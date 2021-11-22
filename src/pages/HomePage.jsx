734975982c99258404d4499436648a7ecaa632b0
leontmd <bankotsu2017@gmail.com>
2021-11-22 11:51:13 -0700

    uncommented the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,21 +7,21 @@ import Auth from '../components/Auth/Auth.jsx';
 export default function HomePage() {
   const [shouldShowScheduler, setShouldShowScheduler] = useState(true);
   // return <Auth />;
-  // return (
-  //   <Grid container sx={{ height: '100%' }}>
-  //     <Grid item xs sx={{ height: '100%', padding: '16px', overflow: 'auto' }}>
-  //       {/* The following is for place-holding only; we should create a component for result/
-  //        * recommendation lists */}
-  //       <Button onClick={() => setShouldShowScheduler(!shouldShowScheduler)}>
-  //         {shouldShowScheduler ? 'Hide' : 'Show'} scheduler
-  //       </Button>
-  //       <CourseCardGrid courses={[1, 2, 3]} />
-  //     </Grid>
-  //     {shouldShowScheduler && (
-  //       <Grid item xs={3} sx={{ margin: '24px' }}>
-  //         <Scheduler />
-  //       </Grid>
-  //     )}
-  //   </Grid>
-  // );
+  return (
+    <Grid container sx={{ height: '100%' }}>
+      <Grid item xs sx={{ height: '100%', padding: '16px', overflow: 'auto' }}>
+        {/* The following is for place-holding only; we should create a component for result/
+         * recommendation lists */}
+        <Button onClick={() => setShouldShowScheduler(!shouldShowScheduler)}>
+          {shouldShowScheduler ? 'Hide' : 'Show'} scheduler
+        </Button>
+        <CourseCardGrid courses={[1, 2, 3]} />
+      </Grid>
+      {shouldShowScheduler && (
+        <Grid item xs={3} sx={{ margin: '24px' }}>
+          <Scheduler />
+        </Grid>
+      )}
+    </Grid>
+  );
 }
