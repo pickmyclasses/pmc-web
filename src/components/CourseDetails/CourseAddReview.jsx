eb897aecc4a5697e70518ed69c2db4c34fb155aa
Qianlang Chen <qianlangchen@utah.edu>
2022-02-03 11:06:09 -0700

    Fix ReviewPage navigation

--- src/components/CourseDetails/CourseAddReview.jsx
+++ src/components/CourseDetails/CourseAddReview.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Grid } from '@mui/material';
 import MuiTypography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import AddCommentIcon from '@mui/icons-material/AddComment';
-import CourseContext from '../../components/CourseDetails/CourseContext';
+import { CourseContext } from '../../pages/CoursePage';
 import { Link } from 'react-router-dom';
 
 export default function CourseAddReview() {
+  const course = useContext(CourseContext);
+
   return (
     <Grid item>
       <MuiTypography variant='h5' gutterBottom>
@@ -17,10 +19,7 @@ export default function CourseAddReview() {
         Share your thoughts with other students
       </MuiTypography>
       <Stack spacing={2} direction='row'>
-        <Link
-          to={`/course/${CourseContext.courseID}/review`}
-          style={{ textDecoration: 'none' }}
-        >
+        <Link to={`/course/${course.ID}/review`} style={{ textDecoration: 'none' }}>
           <Button variant='contained' startIcon={<AddCommentIcon />}>
             Write a Review
           </Button>
