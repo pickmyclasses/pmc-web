0ee9ee2339617ad2695028fbd853e21da8970d59
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-02-15 19:18:27 -0700

    added a table with data

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 const AdminPage = () => {
     return(
-        <h1>Admin Page</h1>
+        <>
+            <h1>Admin Page</h1>
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Class Name</th>
+                    <th>Department</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Object Oriented Programming</td>
+                    <td>Engineer</td>
+                </tr>
+            </table>
+        </>
+
     )
 }
 
