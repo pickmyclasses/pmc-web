3c1838ba5944d086e8cd19709ebb99f887900681
Bettsongy <704443188@qq.com>
2022-02-18 08:58:14 -0700

    implemented recommendation system to the review

---
+++ src/components/CourseDetails/CourseReviewRecommendation.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
+
+function RenderStatus(isRecommended) {
+  let statusTitle;
+  let statusColor;
+  let statusInstruction;
+  if (!isRecommended) {
+    statusTitle = `Don't pick this course!`;
+    statusColor = 'error';
+    statusInstruction = 'The user did not recommend this course.';
+  } else {
+    statusTitle = 'Pick this course!';
+    statusColor = 'success';
+    statusInstruction = 'The user recommended this course.';
+  }
+  return (
+    <Stack sx={{ width: '100%' }} spacing={2}>
+      <Alert variant='outlined' severity={statusColor}>
+        <AlertTitle>{statusTitle}</AlertTitle>
+        <strong>{statusInstruction}</strong>
+      </Alert>
+    </Stack>
+  );
+}
+
+export default function CourseReviewStatus(props) {
+  return RenderStatus(props.isRecommended);
+}
