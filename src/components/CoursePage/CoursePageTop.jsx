32ff197552a46fbd6e8875f0bd1ef5228010c148
Qianlang Chen <qianlangchen@utah.edu>
2022-02-26 15:07:20 -0700

    Fixed course page top bar being on top of nav-bar drop-downs

--- src/components/CoursePage/CoursePageTop.jsx
+++ src/components/CoursePage/CoursePageTop.jsx
@@ -65,7 +65,7 @@ export default function CoursePageTop({ course, tabs, activeTabName }) {
   return (
     <>
       <CardMedia component='img' image={course.ImageURL} height='360px' />
-      <Card sx={{ position: 'sticky', top: 0, zIndex: 9998, boxShadow: 2 }}>
+      <Card sx={{ position: 'sticky', top: 0, zIndex: 1000, boxShadow: 2 }}>
         <Container maxWidth='xl'>
           <Box padding='16px 0' display='flex' justifyContent='space-between'>
             {renderCourseInfo()}
