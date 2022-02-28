4076f3b20509755a7d2a8ff47052ab0276f2165f
Qianlang Chen <qianlangchen@utah.edu>
2022-02-27 23:02:20 -0700

    Added course page scrollbars

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -9,6 +9,7 @@ import CourseRelated from '../components/CoursePage/CourseRelated';
 import CourseReviews from '../components/CoursePage/CourseReviews';
 import CourseRegistration from '../components/CoursePage/CourseRegistration';
 import ContainerWithLoadingIndication from '../components/Page/ContainerWithLoadingIndication';
+import Scrollbars from 'react-custom-scrollbars-2';
 
 export default function CoursePage() {
   const urlParams = useParams();
@@ -30,13 +31,15 @@ export default function CoursePage() {
 
   return (
     <ContainerWithLoadingIndication isLoading={!course || !reviews}>
-      <Box width='100%' height='100%' minHeight={0} sx={{ overflowY: 'scroll' }}>
-        <CoursePageTop course={course} tabs={tabs} activeTabName={activeTabName} />
-        <Container maxWidth='xl' sx={{ paddingY: '32px' }}>
-          <CourseContext.Provider value={{ course, reviews }}>
-            {createElement(tabs[activeTabName].content)}
-          </CourseContext.Provider>
-        </Container>
+      <Box width='100%' height='100%' minHeight={0} sx={{ overflowY: 'sceroll' }}>
+        <OnTopScrollBars>
+          <CoursePageTop course={course} tabs={tabs} activeTabName={activeTabName} />
+          <Container maxWidth='xl' sx={{ paddingY: '32px' }}>
+            <CourseContext.Provider value={{ course, reviews }}>
+              {createElement(tabs[activeTabName].content)}
+            </CourseContext.Provider>
+          </Container>
+        </OnTopScrollBars>
       </Box>
     </ContainerWithLoadingIndication>
   );
@@ -55,4 +58,30 @@ const tabs = {
   'related': { title: 'Related', icon: LocalLibrary, content: CourseRelated },
   'reviews': { title: 'Reviews', icon: Forum, content: CourseReviews },
   'registration': { title: 'Registration', icon: ShoppingCart, content: CourseRegistration },
+};
+
+const OnTopScrollBars = ({ children }) => {
+  /**
+   * For some reason react-custom-scrollbars does not allow "inheriting" styles. It requires
+   * us to always pass in this base styles when styling.
+   * @see https://stackoverflow.com/a/54973078
+   */
+  const defaultScrollBarsStyle = {
+    top: '2px',
+    bottom: '2px',
+    right: '2px',
+    borderRadius: '3px',
+    background: 'rgba(255, 255, 255, 0.111)',
+  };
+
+  return (
+    <Scrollbars
+      autoHide
+      renderTrackVertical={({ style, ...props }) => (
+        <div style={{ ...style, ...defaultScrollBarsStyle, zIndex: 1001 }} {...props} />
+      )}
+    >
+      {children}
+    </Scrollbars>
+  );
 };
