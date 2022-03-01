61e05b7c234a0ea5d4e61abb41aca2af47c6c57c
Bettsongy <704443188@qq.com>
2022-02-28 19:22:30 -0700

    hot fix: fix a bug for piechart where the value keep accumulating

--- src/components/CourseVisuals/ReviewPiechart.jsx
+++ src/components/CourseVisuals/ReviewPiechart.jsx
@@ -1,22 +1,23 @@
 import React, { PureComponent } from 'react';
 import { PieChart, Pie, Cell, Tooltip } from 'recharts';
 
-const data = [
-  { name: 'Five-Star', value: 0 },
-  { name: 'Four-Star', value: 0 },
-  { name: 'Three-Star', value: 0 },
-  { name: 'Two-Star', value: 0 },
-  { name: 'One-Star', value: 0 },
-];
 const COLORS = ['#57e32c', '#b7dd29', '#ffe234', '#ffa534', '#ff4545'];
-function generateStarValues(reviews) {
+function generateStarValues(reviews, data) {
   for (let i = 0; i < reviews.length; i++) {
     data[reviews[i].rating].value += 1;
   }
+  console.log(data);
 }
 
 export default function ReviewPieChart({ reviews }) {
-  generateStarValues(reviews);
+  const data = [
+    { name: 'Five-Star', value: 0 },
+    { name: 'Four-Star', value: 0 },
+    { name: 'Three-Star', value: 0 },
+    { name: 'Two-Star', value: 0 },
+    { name: 'One-Star', value: 0 },
+  ];
+  generateStarValues(reviews, data);
   return (
     <PieChart width={300} height={300}>
       <Pie
