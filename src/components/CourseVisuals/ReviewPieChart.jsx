befc00eba290520ca34745d19820a0878b759234
Kaijie Fu <u1314550@utah.edu>
2022-02-28 21:52:39 -0700

    UPDATE: another major bug fixed

--- src/components/CourseVisuals/ReviewPieChart.jsx
+++ src/components/CourseVisuals/ReviewPieChart.jsx
@@ -17,7 +17,6 @@ export default function ReviewPieChart({ reviews }) {
     { name: 'Four-Star', value: 0 },
     { name: 'Five-Star', value: 0 },
   ];
-
   generateStarValues(reviews, data);
   return (
     <PieChart width={300} height={300}>
