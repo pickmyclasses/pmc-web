176abd1cdf75d953997acbf3c9def15b676910a0
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-03-02 20:19:34 -0700

    styled the Update Data page

--- src/pages/Admin/UpdateData.jsx
+++ src/pages/Admin/UpdateData.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useLocation } from "react-router";
+import "./UpdateData.css";
 
 export default function UpdateData(props)
 {
     //console.log("called from UpdateData");
+    let keyItem = 1;
+
     let location = useLocation();
-    console.log("data passed from AdminPage");
-    console.log(location.state.data);
+
+    let data = location.state.data;
+
+    let dataToRender = [];
+
+    for(let key in data)
+    {
+        let temp = {};
+        temp.key = key;
+        temp.value = data[key];
+        dataToRender.push(temp);
+    }
+
+    let handleChange = () => {
+
+    }
+
+    let handleSubmit = () => {
+
+    }
+
+
     return(
-        <div>Hello world</div>
+        <div className='updateDataDiv'>
+            <h1 style={{textAlign: 'center'}}>Update Data</h1>
+            <form onSubmit={handleSubmit}>
+                {dataToRender.map(obj => <div key={keyItem++}><label>{obj.key}
+                                            {   obj.key === "id" ?  <input className="updateDataInput" type="text" value={obj.value} readOnly/> : 
+                                                obj.value == null ? <input className="updateDataInput" type="text" value={""} onChange={handleChange}/> :
+                                                <input className="updateDataInput" type="text" value={obj.value} onChange={handleChange}/>
+                                            }
+                                            </label><br/>
+                                        </div>)
+                }
+                <input className="updateDateSubmit" type="submit" value="Submit" />
+            </form>
+        </div>
+
+
     )
 }
