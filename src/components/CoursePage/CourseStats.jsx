e9c5ce4b97aa516f8cde67b2f64c1c113789df2a
Bettsongy <704443188@qq.com>
2022-03-02 17:38:11 -0700

    Added tags in the STATS tab to indicate the reviews in each rating scoring bracket.

--- src/components/CoursePage/CourseStats.jsx
+++ src/components/CoursePage/CourseStats.jsx
@@ -4,12 +4,27 @@ import { Grid, Box, Typography, Card } from '@mui/material';
 import { CourseContext } from '../../pages/CoursePage';
 import ReviewDotLineChart from '../CourseVisuals/ReviewDotLineChart';
 import ReviewDotLineFilter from '../CourseVisuals/ReviewDotLineFilter';
+import TagList from '../CourseCardGrid/CourseCard/TagList';
+
+function generateStarValues(reviews, data) {
+  for (let i = 0; i < reviews.length; i++) {
+    data[reviews[i].rating - 1].value += 1;
+  }
+}
 
 export default function CourseStats() {
   const { reviews } = useContext(CourseContext);
   const [filterMethod, setFilterMethod] = useState('This week');
   const [filteredReviews, setFilteredReviews] = useState(reviews);
 
+  const data = [
+    { name: 'Useless (1)', value: 0 },
+    { name: 'Poor (2)', value: 0 },
+    { name: 'Ok (3)', value: 0 },
+    { name: 'Good (4)', value: 0 },
+    { name: 'Excellent (5)', value: 0 },
+  ];
+  generateStarValues(filteredReviews, data);
   useEffect(() => {
     const predicateByFilterMethod = (filterMethod) => {
       const days = daysByFilterMethod[filterMethod];
@@ -30,7 +45,15 @@ export default function CourseStats() {
         <Grid container spacing='32px' marginBottom='32px'>
           <Grid item xs={4}>
             <Card sx={{ width: '100%', height: '100%' }}>
-              <Box sx={{ padding: '12px 24px', '> *': { marginY: '12px !important' } }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  padding: '12px 24px',
+                  '> *': { marginY: '12px !important' },
+                }}
+              >
                 <Typography variant='subtitle2'>Ratings Distribution</Typography>
                 <ReviewPieChart reviews={filteredReviews} />
                 <FilterContext.Provider
@@ -47,9 +70,23 @@ export default function CourseStats() {
           </Grid>
           <Grid item xs={8}>
             <Card sx={{ width: '100%', height: '100%' }}>
-              <Box sx={{ padding: '12px 24px', '> *': { marginY: '12px !important' } }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  padding: '12px 24px',
+                  '> *': { marginY: '12px !important' },
+                }}
+              >
                 <Typography variant='subtitle2'>Ratings Over Time</Typography>
                 <ReviewDotLineChart reviews={filteredReviews} />
+
+                <TagList
+                  tags={data.map((item) => item.name + ' : ' + item.value)}
+                  variant='outlined'
+                  color='success'
+                />
               </Box>
             </Card>
           </Grid>
@@ -57,7 +94,7 @@ export default function CourseStats() {
         <Grid container spacing='32px' marginBottom='32px'>
           <Grid item xs={4}>
             <Card sx={{ width: '100%', height: '300px' }}>
-              <Box sx={{ padding: '12px 24px', '> *': { marginY: '12px !important' } }}></Box>
+              <Box sx={{ padding: '12px 24px', '> *': { marginY: '12px !important' } }}> </Box>
             </Card>
           </Grid>
 
