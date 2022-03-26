37195c79f19e1404c5faa5a61bd4f503da40e813
Qianlang Chen <qianlangchen@utah.edu>
2022-03-26 01:26:35 -0600

    in->is

--- src/components/ProfilePage/ProfileAvatarDisplay.jsx
+++ src/components/ProfilePage/ProfileAvatarDisplay.jsx
@@ -13,7 +13,7 @@ export default function ProfileAvatarDisplay() {
         sx={{ width: '120px', height: '120px', marginBottom: '16px' }}
         src='https://i.insider.com/60817ec5354dde0018c06960?width=700'
       >
-        {/* The following in the default gray avatar that would become visible without the
+        {/* The following is the default gray avatar that would become visible without the
          *  the `src` prop in the Avatar above. */}
         <Person sx={{ transform: 'scale(3.5)' }} />
       </Avatar>
