4bc8c31ecce5e714a1eea7d6c99efd3f8d0c3c4b
Qianlang Chen <qianlangchen@utah.edu>
2022-03-27 00:49:07 -0600

    UPDATE: nav bar schedule button always links to sket page (<-- open for name suggestions)

--- src/components/NavigationBar/NavigationBarButtonGroup/SchedulerDropDown.jsx
+++ src/components/NavigationBar/NavigationBarButtonGroup/SchedulerDropDown.jsx
@@ -1,8 +1,6 @@
 import { EventNote } from '@mui/icons-material';
-import { Menu } from '@mui/material';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import PreventableLink from 'components/PreventableNavigation/PreventableLink';
 import React from 'react';
-import Scheduler from '../../Scheduler/Scheduler';
 import { ButtonGroupTab } from '../NavigationBarButtonGroup';
 
 export default function SchedulerDropDown() {
@@ -14,27 +12,37 @@ export default function SchedulerDropDown() {
   // if (hasStaticScheduler) setShouldShowStaticScheduler(!shouldShowStaticScheduler);
   // };
 
+  // Update 03/27/22: this scheduler button always links to the schedule detail page.
+  // return (
+  //   <PopupState variant='popover'>
+  //     {(popupState) => (
+  //       <>
+  //         <ButtonGroupTab label='Schedule' icon={<EventNote />} {...bindTrigger(popupState)} />
+  //         <Menu
+  //           {...bindMenu(popupState)}
+  //           sx={{ '> *': { overflow: 'visible' } }}
+  //           PaperProps={{
+  //             sx: {
+  //               overflow: 'visible',
+  //               width: '360px',
+  //               height: '792px',
+  //               '> *': { padding: '20px !important', height: 'calc(100% - 40px)' },
+  //             },
+  //           }}
+  //         >
+  //           <Scheduler />
+  //         </Menu>
+  //       </>
+  //     )}
+  //   </PopupState>
+  // );
+
   return (
-    <PopupState variant='popover'>
-      {(popupState) => (
-        <>
-          <ButtonGroupTab label='Schedule' icon={<EventNote />} {...bindTrigger(popupState)} />
-          <Menu
-            {...bindMenu(popupState)}
-            sx={{ '> *': { overflow: 'visible' } }}
-            PaperProps={{
-              sx: {
-                overflow: 'visible',
-                width: '360px',
-                height: '792px',
-                '> *': { padding: '20px !important', height: 'calc(100% - 40px)' },
-              },
-            }}
-          >
-            <Scheduler />
-          </Menu>
-        </>
-      )}
-    </PopupState>
+    <ButtonGroupTab
+      label='Schedule'
+      icon={<EventNote />}
+      component={PreventableLink}
+      to='/profile/schedule'
+    />
   );
 }
