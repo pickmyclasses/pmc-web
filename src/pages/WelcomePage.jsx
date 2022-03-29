18a8f30654ed6581a9dbfd14eb3599c2386cb9ba
Kaijie Fu <u1314550@utah.edu>
2022-03-29 01:25:42 -0600

    fixed overflow

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -18,6 +18,7 @@ const useStyle = makeStyles({
     fontFamily:
       '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
     display: 'flex',
+    overflow: 'auto',
   },
   mainForm: {
     width: '70%',
