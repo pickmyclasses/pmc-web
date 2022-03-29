1c8b0b1c376a7e6d042938276b7f59be43a37817
Kaijie Fu <u1314550@utah.edu>
2022-03-29 00:31:40 -0600

    removed useless file

--- src/components/CourseReviews/CourseReviewComposer.jsx
+++
@@ -1,8 +0,0 @@
-import React, {useState} from 'react';
-import Box from '@mui/material/Box';
-
-export default function CourseReviewComposer() {
-	const [anonymous, setAnonymous] = useState(true)
-	const [recommend, setRecommend] = useState(true)
-	const 
-}
