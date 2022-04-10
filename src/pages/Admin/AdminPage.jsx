abc50a187f930607a92dea4814741548b33b047e
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-10 17:18:26 -0600

    updated table options

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -19,13 +19,24 @@ export default function AdminPage(props)
         <label className="selector-label">Choose a table:</label>
 
         <select name="tables" id="tables-selector" onChange={handleChange}>
-            <option value="college">college</option>
+            <option value="building">building</option>
             <option value="class">class</option>
+            <option value="college">college</option>
+            <option value="course">course</option>
+            <option value="course_set">course_set</option>
+            <option value="custom_event">custom_event</option>
+            <option value="major">major</option>
+            <option value="over_all_rating">over_all_rating</option>
+            <option value="prerequisites">prerequisites</option>
             <option value="professor">professor</option>
-            <option value="user">user</option>
             <option value="review">review</option>
             <option value="schedule">schedule</option>
-            <option value="feedback">feedback</option>
+            <option value="semester">semester</option>
+            <option value="subject">subject</option>
+            <option value="tag">tag</option>
+            <option value="user">user</option>
+            <option value="user_course_history">user_course_history</option>            
+            <option value="user_voted_tag">user_voted_tag</option>
         </select>
         <div style={{marginBottom: '20px', marginTop: '20px'}}>---------------------------------------------------</div>
 
