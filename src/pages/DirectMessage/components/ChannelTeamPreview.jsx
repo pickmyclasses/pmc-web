e836e411e15e76e11b33a9c5cf0ee8584b2a029c
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-11 23:35:52 -0600

    added comments

--- src/pages/DirectMessage/components/ChannelTeamPreview.jsx
+++ src/pages/DirectMessage/components/ChannelTeamPreview.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import { Avatar, useChatContext } from 'stream-chat-react';
 
-const ChannelTeamPreview = ({}) => {
+const ChannelTeamPreview = ({channel, type}) => 
+{
+    // re-name channel to activeChannel because we already have channel at the top
+    const { channel: activeChannel, client } = useChatContext();
+
+    const ChannelPreview = () => {
+        <p className='channel-preview__item'>
+            # {channel?.data?.name || channel?.data?.id}
+        </p>
+    }
+
+    // => () returns object
+    // => {} function body  
+    const DirectPreview = () => 
+    {
+        // filter is a function that takes a callback function with the parameter of { user } 
+        // and returns an array where user.id !== client.userID
+        const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
+
+        return(
+            <div className='channel-preview__item single'>
+                <Avatar
+                    image={members[0]?.user?.image}
+                    name={members[0]?.user?.fullName}
+                    size={24}
+                />
+                <p>{members[0]?.user?.fullName}</p>
+            </div>
+        )
+    }
+
     return(
         <div></div>
     )
