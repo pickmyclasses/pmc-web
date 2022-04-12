55784f9e8462e8fb1edda91a3ef7442e09f4c9cb
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-12 10:01:02 -0600

    finished front-end auth

--- src/pages/DirectMessage/components/Auth.jsx
+++ src/pages/DirectMessage/components/Auth.jsx
@@ -4,12 +4,34 @@ import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg';
 
+const initialState = {
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    avatarURL: '' 
+}
+
 const Auth = () => {
 
+    const [form, setForm] = useState(initialState);
     const [isSignup, setIsSignup] = useState(true);
 
-    const handleChange = () => {
+    const handleChange = (e) => {
+        // the square bracket is just a syntax matter
+        // spreading the field with respect to e.target.name of form
+        setForm({...form, [e.target.name]: e.target.value});
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(form);
+    }
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
     }
 
     return(
@@ -17,7 +39,7 @@ const Auth = () => {
             <div className='auth__form-container_fields'>
                 <div className='auth__form-container_fields-content'>
                     <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
-                    <form onSubmit={() => {}}>
+                    <form onSubmit={handleSubmit}>
                         {isSignup && (
                             <div className='auth__form-container_fields-content_input'>
                                 <label htmlFor="fullName">Full Name</label>
@@ -64,8 +86,44 @@ const Auth = () => {
                                 />
                             </div>
                         )}
+                        <div className='auth__form-container_fields-content_input'>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className='auth__form-container_fields-content_button'>
+                            <button>{isSignup ? 'Sign Up' : 'Sign In'}</button>
+                        </div>
                     </form>
+                    <div className='auth__form-container_fields-account'>
+                        <p>
+                            {isSignup ? "Already have an account? " : "Don't have an account? "}  
+                            <span onClick={switchMode}>
+                                {isSignup ? 'Sign In' : 'Sign Up'}
+                            </span>
+                        </p>
+                    </div>
                 </div>
+            </div>
+            <div className='auth__form-container_image'>
+                <img src={signinImage} alt="sign in" />
             </div>
         </div>
     )
