bb9a0ec46e97b9504dcb7d99ca77f972b3aa15b2
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-12 09:37:51 -0600

    added more fields for Auth

--- src/pages/DirectMessage/components/Auth.jsx
+++ src/pages/DirectMessage/components/Auth.jsx
@@ -6,10 +6,10 @@ import signinImage from '../assets/signup.jpg';
 
 const Auth = () => {
 
-    const [isSignup, setIsSignup] = useState(false);
+    const [isSignup, setIsSignup] = useState(true);
 
     const handleChange = () => {
-        
+
     }
 
     return(
@@ -20,11 +20,45 @@ const Auth = () => {
                     <form onSubmit={() => {}}>
                         {isSignup && (
                             <div className='auth__form-container_fields-content_input'>
-                                <label for="fullName">Full Name</label>
+                                <label htmlFor="fullName">Full Name</label>
                                 <input
                                     name="fullName"
                                     type="text"
                                     placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor="fullName">Username</label>
+                                <input
+                                    name="username"
+                                    type="text"
+                                    placeholder="Username"
+                                    onChange={handleChange}
+                                    required
+                                />
+                        </div>
+                        {isSignup && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor="phoneNumber">Phone Number</label>
+                                <input
+                                    name="phoneNumber"
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {isSignup && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor="avatarURL">Avatar URL</label>
+                                <input
+                                    name="avatarURL"
+                                    type="text"
+                                    placeholder="Avatar URL"
                                     onChange={handleChange}
                                     required
                                 />
