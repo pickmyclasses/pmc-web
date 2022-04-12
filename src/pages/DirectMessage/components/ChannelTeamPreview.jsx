868f7f74bf9ba521166c8935306e56404397f0b4
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-12 08:43:26 -0600

    finished ChannelTeamPreview

--- src/pages/DirectMessage/components/ChannelTeamPreview.jsx
+++ src/pages/DirectMessage/components/ChannelTeamPreview.jsx
@@ -33,7 +33,11 @@ const ChannelTeamPreview = ({channel, type}) =>
     }
 
     return(
-        <div></div>
+        <div className={channel?.id === activeChannel?.id ? 'channel-preview__wrapper__selected' : 'channel-preview__wrapper'} 
+             onClick={() => {console.log(channel)}}
+        >
+            {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
+        </div>
     )
 };
 
