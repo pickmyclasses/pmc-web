3b53576ea7f606c736cd34c71745079a81e7b06d
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-11 20:26:31 -0600

    installed new package and created Main.jsx

---
+++ src/pages/DirectMessage/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>Direct Message Admin</h1>
+        </div>
+    )
+}
+
+export default Main;
