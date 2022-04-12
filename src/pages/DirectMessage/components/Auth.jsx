b3cafc193f44945d46fac9a752588433bbc06c54
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-12 09:21:45 -0600

    finished the view of Auth.jsx

--- src/pages/DirectMessage/components/Auth.jsx
+++ src/pages/DirectMessage/components/Auth.jsx
@@ -5,8 +5,35 @@ import axios from 'axios';
 import signinImage from '../assets/signup.jpg';
 
 const Auth = () => {
+
+    const [isSignup, setIsSignup] = useState(false);
+
+    const handleChange = () => {
+        
+    }
+
     return(
-        <div></div>
+        <div className='auth__form-container'>
+            <div className='auth__form-container_fields'>
+                <div className='auth__form-container_fields-content'>
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={() => {}}>
+                        {isSignup && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label for="fullName">Full Name</label>
+                                <input
+                                    name="fullName"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 };
 
