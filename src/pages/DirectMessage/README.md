3e592253cfd2723e51058cd2e3e158c0193707d4
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-12 22:05:02 -0600

    updated README for direct-message

--- src/pages/DirectMessage/README.md
+++ src/pages/DirectMessage/README.md
@@ -1,3 +1,7 @@
 # TODO:
 - refractor css file
 - documenting the code
+
+- Flow of the program:
+    - DirectMessage.jsx -> ContainerChannelList.jsx -> ChannelTeamList.jsx and ChannelTeamPreview.jsx
+    - TODO: DirectMessage.jsx -> ContainerChannel.jsx -> InnerChannel.jsx, ChannelCreate.jsx, ChannelEdit.jsx -> ListUsers.jsx
--- src/pages/DirectMessage/components/index.js
+++ src/pages/DirectMessage/components/index.js
@@ -6,5 +6,4 @@ export { default as AdminAuthentication } from './AdminAuthentication';
 export { default as InnerChannel } from './InnerChannel';
 export { default as ChannelCreate } from './ChannelCreate';
 export { default as ChannelEdit } from './ChannelEdit';
-export { default as Message } from './Message';
 export { default as ListUsers } from './ListUsers';
