fec5db5d367c23c0d514101beed985550f967ec0
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-12 19:11:29 -0600

    changed the pager

--- src/pages/DirectMessage/components/ContainerChannelList.jsx
+++ src/pages/DirectMessage/components/ContainerChannelList.jsx
@@ -25,7 +25,7 @@ const SideBar = ({ logout }) => (
 
 const CompanyHeader = () => (
     <div className="channel-list__header">
-        <p className="channel-list__header__text">PickMyClasses</p>
+        <p className="channel-list__header__text">Administrators</p>
     </div>
 )
 
