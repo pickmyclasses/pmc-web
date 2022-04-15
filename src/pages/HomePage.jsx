96d4c51a535fce05f13bb3a27739b0dab5e238cd
Qianlang Chen <qianlangchen@utah.edu>
2022-04-15 00:29:17 -0600

    Home page: Functional by major

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,21 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { fetchHomePageCourses } from '../api';
 import ContainerWithStaticScheduler from '../components/Scheduler/ContainerWithStaticScheduler';
-import { useMount } from '../utils';
 import CourseCardGrid from '../components/CourseCardGrid/CourseCardGrid';
 import ClickableIndicator from '../components/CourseCardGrid/CourseCard/ClickableIndicator';
 import { Box, Divider, Typography, colors } from '@mui/material';
 import { NavigationBarContext } from '../components/NavigationBar/ContainerWithNavigationBar';
 import Scrollbars from 'react-custom-scrollbars-2';
 import ContainerWithLoadingIndication from '../components/Page/ContainerWithLoadingIndication';
+import { UserContext } from 'App';
 
 export default function HomePage() {
+  const { user } = useContext(UserContext);
   const { shouldShowStaticScheduler } = useContext(NavigationBarContext);
 
   const [courseCategories, setCourseCategories] = useState(null);
 
-  useMount(() => fetchHomePageCourses().then(setCourseCategories));
+  useEffect(() => user && fetchHomePageCourses(user.userID).then(setCourseCategories), [user]);
 
   return (
     <ContainerWithStaticScheduler>
@@ -22,17 +23,17 @@ export default function HomePage() {
         <Scrollbars autoHide>
           <Box padding='32px'>
             {courseCategories &&
-              courseCategories.map(({ category, courses }, i) => (
+              courseCategories.map(({ directCourseSetName, courseList }, i) => (
                 <Box key={i}>
                   {i > 0 && <Divider sx={{ marginY: '8px' }} />}
                   <ClickableIndicator>
-                    <SectionOverline>{category}</SectionOverline>
+                    <SectionOverline>{directCourseSetName}</SectionOverline>
                   </ClickableIndicator>
                   <Box sx={{ padding: '8px 0 16px 0' }}>
                     <CourseCardGrid
                       key={i}
-                      title={category}
-                      courses={courses}
+                      title={directCourseSetName}
+                      courses={courseList}
                       numColumns={shouldShowStaticScheduler ? 4 : 5}
                     />
                   </Box>
