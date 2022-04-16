77a3164dc19531fd075e45f7472902cd8a215398
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-15 20:31:27 -0600

    added comments in UpdateData.jsx

--- src/pages/Admin/UpdateData/UpdateData.jsx
+++ src/pages/Admin/UpdateData/UpdateData.jsx
@@ -4,62 +4,79 @@ import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import './UpdateData.css';
 
-export default function UpdateData() {
-  //console.log("called from UpdateData");
-  let keyItem = 1;
-
+export default function UpdateData() 
+{
+  // location is the place that stored the data when the previous page click the update button
   let location = useLocation();
 
+  // fetch the data from location 
   let data = location.state.data;
 
+  // tableName is a property
   const [tableName, setTableName] = useState(location.state.table);
 
-  console.log(tableName);
-
+  // array that contains the fields (columns) and the value that initially render
   let dataToRender = [];
 
-  for (let key in data) {
+  // populate data to render
+  for (let key in data) 
+  {
     let temp = {};
     temp.key = key;
     temp.value = data[key];
     dataToRender.push(temp);
   }
 
-  let handleChange = (key, e) => {
+  // handle change means update the value with the respective key in data array
+  // with the new value that the user typed in
+  let handleChange = (key, e) => 
+  {
     data[key] = e.target.value;
   };
 
-  let handleSubmit = (e) => {
+  // handleSubmit is called when the user submit the form
+  let handleSubmit = (e) => 
+  {
     e.preventDefault();
-    console.log(data);
-
     let request_body = data;
+    // add a field table_name = tableName
     request_body.table_name = tableName;
 
-    const requestOptions = {
+    // request body and header and method type
+    const requestOptions = 
+    {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(request_body),
     };
 
     // let URL = 'https://pmc-schedule-api.herokuapp.com/update-data';
-     let URL = 'http://localhost:5002/update-data';
-    console.log(URL);
+
+    // the endpoint where the api is attached
+    let URL = 'http://localhost:5002/update-data';
+
+    // call the api to post the updated data
     fetch(URL, requestOptions)
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          alert(data.message);
-        },
-        (error) => {
+    .then((res) => res.json())
+    .then(
+          (data) => 
+          {
+            alert(data.message);
+          },
+        (error) => 
+        {
           alert(error);
         }
       );
   };
 
+  // keyItem because the map method requires key
+  let keyItem = 1;
+
   return (
     <div className='updateDataDiv'>
       <h1 style={{ textAlign: 'center' }}>Update Data</h1>
+      {/* This button goes back to the Admin Data Table */}
       <Link to="/admin">
         <Button
           variant="contained"
@@ -68,11 +85,14 @@ export default function UpdateData() {
           Admin Data Table
         </Button>
       </Link>
+      {/* The form */}
       <form onSubmit={handleSubmit} style={{ marginTop: '50px' }}>
         {dataToRender.map((obj) => (
           <div key={keyItem++}>
+            {/* The label */}
             <label>
               {obj.key}
+              {/* If the field is id then the user can't type (readonly). Otherwise they can */}
               {obj.key === 'id' ? (
                 <input
                   className='updateDataInput'
@@ -92,6 +112,7 @@ export default function UpdateData() {
             <br />
           </div>
         ))}
+        {/*Submit button */}
         <input className='updateDateSubmit' type='submit' value='Submit' />
       </form>
     </div>
--- src/pages/DirectMessage/README.md
+++
@@ -1,9 +0,0 @@
-# TODO:
-- refractor css file: TODO:  AdminAuthentication
-
-
-- documenting the code
-
-- Flow of the program:
-    - DirectMessage.jsx -> ContainerChannelList.jsx -> ChannelTeamList.jsx and ChannelTeamPreview.jsx
-    - TODO: DirectMessage.jsx -> ContainerChannel.jsx -> ChannelCore.jsx, ChannelCreate.jsx, ChannelEdit.jsx -> ListUsers.jsx
